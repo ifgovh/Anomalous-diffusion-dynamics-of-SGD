0646860cdfb468c750825e50b15c53d9b4d23d1d
ifgovh <ifgovh@163.com>
2020-12-13 23:35:55 +1100

    update

--- GPU_train.sh
+++ GPU_train.sh
@@ -7,7 +7,7 @@
 #PBS -e PBSout/
 #PBS -o PBSout/
 ##PBS -J 1-2
-PBS_ARRAY_INDEX=1
+PBS_ARRAY_INDEX=2
 cd ~
 source tf/bin/activate
 cd "$PBS_O_WORKDIR"
@@ -15,7 +15,7 @@ params=`sed "${PBS_ARRAY_INDEX}q;d" job_params`
 param_array=( $params )
 
 # training DNN
-for i in {4..10}
+for i in {1..10}
 do
 	python -m main --model=${param_array[i*5]} --lr=${param_array[i*5+1]} --epochs=${param_array[i*5+2]}  --batch_size=${param_array[i*5+3]} --dataset=${param_array[i*5+4]}
 done
